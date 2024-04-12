94179257f952d80e1d3a401ff83368b590b0e4de
Jordan <jormmiller@gmail.com>
2024-04-12 13:59:52 -0400

    changed inquirer version. added validation to user input

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ const questions = [
     {
         type: 'input',
         name: 'size',
-        message: 'What size would you like your logo to be?'
+        message: 'What size would you like your logo to be?',
+        validate: (value) => Number.isInteger(Number(value)) ? true : 'Please enter a valid number.'
+        
     },
     {
         type: 'input',
         name: 'text',
-        message: 'Enter up to 3 characters for your logo?'
+        message: 'Enter up to 3 characters for your logo?',
+        validate: (value) => value.length <= 3 ? true : 'Text must be up to 3 characters long.'
     },
     {
         type: 'input',
         name: 'textColor',
-        message: 'What color would you like your logo text to be? (keyword or hex)'
+        message: 'What color would you like your logo text to be? (keyword or hex)',
+        validate: (value) => isValidColor(value) ? true : 'Please enter a valid color or 6-character hex value.'
     },
     {
         type: 'list',
@@ -29,17 +33,32 @@ const questions = [
     {
         type: 'input',
         name: 'shapeColor',
-        message: 'What is the color of the shape? (keyword or hex)'
+        message: 'What color would you like your shape to be? (keyword or hex)',
+        validate: (value) => isValidColor(value) ? true : 'Please enter a valid color or 6-character hex value.'
     },
 ];
 
-const circle = new Circle('red', 300);
-const square = new Square('red', 300);
-const triangle = new Triangle('red', 300);
+function isValidColor(value) {
+    let colorRegex = new RegExp(/^[a-zA-Z]+$/);
+    let hexRegex = new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
+    return colorRegex.test(value) || hexRegex.test(value);
+}
 
-console.log(circle.render());
-console.log(square.render());
-console.log(triangle.render());
+async function getAnswers() {
+    return inquirer.prompt(questions);
+}
+
+function renderLogo(answers) {
+    console.log('Rendering logo...');
+}
+
+// const circle = new Circle('red', 300);
+// const square = new Square('red', 300);
+// const triangle = new Triangle('red', 300);
+
+// console.log(circle.render());
+// console.log(square.render());
+// console.log(triangle.render());
 
 // const svg = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
 // ${circle.render()}
@@ -47,8 +66,11 @@ console.log(triangle.render());
 // ${triangle.render()}
 // </svg>`;
 
-const svg = `<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-    ${circle.render()}
-</svg>`;
+// const svg = `<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+//     ${triangle.render()}
+// </svg>`;
 
-console.log(svg);
+getAnswers().then(answers => {
+    console.log(answers);
+    renderLogo(answers);
+});
